da6c362b4c658ccd0ddbf535bc3458fcf7184017
Hanane Ladj <hananeladj@gmail.com>
2020-10-01 15:21:25 -0700

    final adjustement

--- js/gallerySearch.js
+++ js/gallerySearch.js
@@ -1,51 +1,21 @@
 
-// const searchBar = document.getElementById('search');
-// let gallery = document.getElementsByClassName('gallery-pic');
+const searchBar = document.getElementById('search');
+let gallery = document.getElementsByClassName('gallery-pic');
 
 
-// document.getElementById('search').addEventListener('keyup', function(e) {
-//     const searchBar = document.getElementById('search');
-//     let searchInput = e.target.value.toUpperCase();
+document.getElementById('search').addEventListener('keyup', function(e) {
+    let searchBar = document.getElementById('search');
+    searchBar.value = searchBar.value.toUpperCase();
+    let searchInput = e.target.value.toUpperCase();
 
-//     for (i = 0; i < gallery.length; i++) {
-//         const searchField = gallery[i].getAttribute('alt').toUpperCase()
-//         if (searchField.includes(searchInput)) {
-//             gallery[i].style.display = '';
-//         } else {
-//             gallery[i].style.display = 'none';
-//         }
-//     }
-// })
-
-// function gallerySearch() {
-//     var searchbar = document.getElementById('search');
-//     searchBar.value = searchBar.value.toUpperCase();
-//     let searchImg = document.getElementsByClassName('gallery-pic');
-//     for (i = 0; i < searchImg.length; i++) {
-//         if (searchImg[i].getAttribute('alt').toUpperCase()) {
-//             searchImg[i].style.display = "";
-
-//         } else {
-//             searchImg.style.display = "none";
-//         }
-//     }
-// }
-
-
-document.getElementById("fname").addEventListener("keyup", gallerySearch);
-
-function gallerySearch() {
-  let searchBar = document.getElementById("search");
-  searchBar.value = searchBar.value.toUpperCase();
-  let searchImg = document.getElementsByClassName('gallery-pic');
-    for (i = 0; i < searchImg.length; i++) {
-        if (searchImg[i].getAttribute('alt')) {
-            searchImg[i].style.display = "";
-
+    for (i = 0; i < gallery.length; i++) {
+        const searchField = gallery[i].getAttribute('data-caption').toUpperCase()
+        if (searchField.includes(searchInput)) {
+            gallery[i].style.display = '';
         } else {
-            searchImg.style.display = "none";
+            gallery[i].style.display = 'none';
         }
     }
-
    
-}
+})
+
